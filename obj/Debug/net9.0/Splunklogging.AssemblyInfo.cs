078273b4984154e964ff50f04289a548907bc088
dqube <mdevendran@outlook.com>
2025-02-18 22:40:39 -0400

    gitignore added

--- obj/Debug/net9.0/Splunklogging.AssemblyInfo.cs
+++ obj/Debug/net9.0/Splunklogging.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Splunklogging")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f122b9803d326d93c34683aff18a301ed2f02435")]
 [assembly: System.Reflection.AssemblyProductAttribute("Splunklogging")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Splunklogging")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
